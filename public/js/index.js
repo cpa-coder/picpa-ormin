d177f503a59f41caad35312414f23220027e888a
Ogie Galicia <o.galicia18@gmail.com>
2021-08-12 21:48:49 +0800

    Feat: basic setup

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,17 @@
-function toggleNavBar() {
-    const x = document.getElementById('myDIV');
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+function toggleMenu() {
+    document.getElementById('menu').classList.toggle('show');
 }
+
+window.onclick = function (event) {
+    if (event.target.matches('.drop-btn')) {
+        return;
+    }
+
+    const dropdowns = document.getElementsByClassName('dropdown-content');
+    for (let i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+    }
+};
